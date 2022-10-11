1c2e7c4746d3ea18356e95bef40b7470f8a1b9f2
DanielYesteLopez <dyestelo10@alumnes.ub.edu>
2022-10-11 23:33:57 +0200

    ·Database model to be checked. Created different fields that can fit in the project idea. We will have a many-to-one-relationship between the housing and the users. Each user can have different houses but a house can only have a user(owner). ·In my opininion, there are no more helpful fields for the project at the moment. The relationship can be argued but feels logical to me.

--- doogkingapp/models.py
+++ doogkingapp/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    user_id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=50)
+    user_mail = models.CharField(max_length=50)
+    password = models.CharField(max_length=50)
+    street = models.CharField(max_length=50)
+    street_number = models.IntegerField()
+
+class Housing(models.Model):
+    house_id = models.IntegerField(primary_key=True)
+    street = models.CharField(max_length=50)
+    street_number = models.IntegerField()
+    house_dimension = models.IntegerField()
+    house_owner = models.ForeignKey(User, on_delete=models.CASCADE)
