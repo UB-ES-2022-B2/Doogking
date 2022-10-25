0e6c48abf50bbb5c1f27ab8c01638a33cac1061c
auram <auramch98@gmail.com>
2022-10-25 19:46:11 +0200

    Backend: implementación restricciones formulario

--- doogkingapp/managers.py
+++ doogkingapp/managers.py
@@ -1,10 +1,27 @@
 from django.contrib.auth.base_user import BaseUserManager
+from django.core.validators import validate_email
+from django.core.exceptions import ValidationError
 
 
 class ProfileManager(BaseUserManager):
     def create_user(self,email, password, **extra_fields):
         if not email:
             raise ValueError('The Email must be set')
+        if not validate_email(email):
+            raise ValueError('The email is not correct')
+
+        if not len(password) > 8:
+            raise ValueError('Password length should be at least 8')
+
+        if not any(char.isdigit() for char in password):
+            raise ValueError('Password should have at least one number')
+
+        if not any(char.isupper() for char in password):
+            raise ValueError('Password should have at least one uppercase letter')
+
+        if not any(char.islower() for char in password):
+            raise ValueError('Password should have at least one lowercase letter')
+
         email = self.normalize_email(email)
         user = self.model(email=email, **extra_fields)
         user.set_password(password)
