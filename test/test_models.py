ada87c385f552a67de6f1aed776cd0edf9ff5728
auram <auramch98@gmail.com>
2022-10-26 00:49:11 +0200

    Tests nuevas restricciones Backend

--- test/test_models.py
+++ test/test_models.py
@@ -4,7 +4,7 @@ from doogkingapp.models import Profile
 class ModelsTestCase(TestCase):
 
     def create_Profile(self):
-        return Profile.objects.create_user(email = "test@gmail.com", password= "password123")
+        return Profile.objects.create_user(email = "test@gmail.com", password= "Password123")
 
     def test_profile_creation(self):
         user = self.create_Profile()
@@ -12,10 +12,37 @@ class ModelsTestCase(TestCase):
         self.assertEqual(user.__str__(), user.email)
 
     def create_superuser(self):
-        return Profile.objects.create_superuser(email = "superusertest@gmail.com", password= "password123",is_staff= True,is_superuser= True,is_active=True)
+        return Profile.objects.create_superuser(email = "superusertest@gmail.com", password= "Password123",is_staff= True,is_superuser= True,is_active=True)
 
     def test_superuser_creation(self):
         super_user= self.create_superuser()
         self.assertTrue(isinstance(super_user,Profile))
         self.assertEqual(super_user.__str__(), super_user.email)
+
+    def test_wrong_email(self):
+        with self.assertRaises(ValueError) as ex:
+            Profile.objects.create_user(email="testgmailcom", password="Password123")
+        self.assertEqual('The email is not correct',str(ex.exception))
+
+    def test_wrong_password_length(self):
+        with self.assertRaises(ValueError) as ex:
+            Profile.objects.create_user(email="test@gmail.com", password="Pass")
+        self.assertEqual('Password length should be at least 8',str(ex.exception))
+
+    def test_wrong_password_number(self):
+        with self.assertRaises(ValueError) as ex:
+            Profile.objects.create_user(email="test@gmail.com", password="Passwordasdf")
+        self.assertEqual('Password should have at least one number',str(ex.exception))
+
+
+    def test_wrong_password_uppercase(self):
+        with self.assertRaises(ValueError) as ex:
+            Profile.objects.create_user(email="test@gmail.com", password="password123")
+        self.assertEqual('Password should have at least one uppercase letter',str(ex.exception))
+
+    def test_wrong_password_lowercase(self):
+        with self.assertRaises(ValueError) as ex:
+            Profile.objects.create_user(email="test@gmail.com", password="PASSWORD123")
+        self.assertEqual('Password should have at least one lowercase letter',str(ex.exception))
+
 
