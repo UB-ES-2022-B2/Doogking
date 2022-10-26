50323610d2f330ff9783c2e715db62bd2f47988e
DanielYesteLopez <dyestelo10@alumnes.ub.edu>
2022-10-26 17:24:57 +0200

    *Creación de ForgotPassword.vue y Reset.vue. Dichos componentes están en fase de test para poder tanto recibir como enviar correos y habilitar la recuperación de contraseña.

---
+++ frontend/src/components/Reset.vue
@@ -0,0 +1,14 @@
+<template>
+  <form>
+    <h3>Reset Password</h3>
+      <div class="form-group">
+        <label>Password</label>
+        <input type="password" class="form-control" placeholder="Password"/>
+      </div>
+    <div class="form-group">
+      <label>Password confirm</label>
+      <input type="password" class="form-control" placeholder="Password confirm"/>
+    </div>
+    <button class="btn btn-primary btn-block">Submit</button>
+  </form>
+</template>
