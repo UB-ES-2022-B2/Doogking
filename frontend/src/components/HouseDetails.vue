aa067e8b9ecdc4a3be4787a2ae831b61071fa744
PauRodriguezTrens <robluccione1963@gmail.com>
2022-11-24 20:36:08 +0100

    Added card with details on HouseDeails.vue.

--- frontend/src/components/HouseDetails.vue
+++ frontend/src/components/HouseDetails.vue
@@ -1,23 +1,44 @@
 <template>
   <div class="flex-wrapper">
     <Header></Header>
-    <Galleria id="galleriaHouse" :value="images" :responsiveOptions="responsiveOptions" :numVisible="3"
-              :showItemNavigators="true" :showItemNavigatorsOnHover="true"
-              :circular="true" :autoPlay="true" :transitionInterval="3000"
-              style="max-width: 39em; margin:1em;">
-      <template #item="{item}">
-        <img id="imageHouse" :src="item.itemImageSrc" :alt="item.alt" style="width: 100%; display: block;" />
-      </template>
-      <template #thumbnail="{item}">
-        <div class="grid grid-nogutter justify-content-center">
-          <img id="thumbnailImage" :src="item.thumbnailImageSrc" :alt="item.alt" style="display: block;" />
+    <div id="houseContainer">
+      <div id="galleriaContainer" class="houseDetails">
+        <Galleria id="galleriaHouse" :value="images" :responsiveOptions="responsiveOptions" :numVisible="3"
+                  :showItemNavigators="true" :showItemNavigatorsOnHover="true"
+                  :circular="true" :autoPlay="true" :transitionInterval="3000"
+                  style="max-width: 39em; margin:1em;">
+          <template #item="{item}">
+            <img id="imageHouse" :src="item.itemImageSrc" :alt="item.alt" style="width: 100%; display: block;" />
+          </template>
+          <template #thumbnail="{item}">
+            <div class="grid grid-nogutter justify-content-center">
+              <img id="thumbnailImage" :src="item.thumbnailImageSrc" :alt="item.alt" style="display: block;" />
+            </div>
+          </template>
+          <template #caption="{item}">
+            <h4 style="margin-bottom: .5rem;">{{house.city}}</h4>
+            <p>{{house.street}},{{house.street_number}},{{house.floor}},{{house.door}},{{house.house_dimension}}</p>
+          </template>
+        </Galleria>
+      </div>
+      <div id="detailsContainer" class="houseDetails">
+        <div class="form-demo">
+          <div class="flex justify-content-center">
+            <div class="card">
+              <label class="text-center" style="margin-top: 1em; font-weight: 600; font-size: 20px;">{{ house.city}} <a style="text-decoration: none; font-size: 15px; color: #a0a0a0;">{{house.street}}, {{house.street_number}}, {{house.floor}}, {{house.door}}, {{house.house_dimension}}</a></label>
+              <form @submit.prevent="handleSubmit(!v$.$invalid)" class="p-fluid">
+                <div class="field" style="margin-top: -1em">
+                  <Button id="submitButton" type="submit" label="Reserve" class="mt-2"/>
+                </div>
+                <div class="field" style="margin-top: -1em">
+                  <label>Want to reserve? <a class="link" @click="goToLogin" style="cursor: pointer; color: #8DD0FF; text-decoration: none; margin-top: -1em">Login now!</a></label>
+                </div>
+              </form>
+            </div>
+          </div>
         </div>
-      </template>
-      <template #caption="{item}">
-        <h4 style="margin-bottom: .5rem;">{{house.city}}</h4>
-        <p>{{house.street}},{{house.street_number}},{{house.floor}},{{house.door}},{{house.house_dimension}}</p>
-      </template>
-    </Galleria>
+      </div>
+    </div>
     <Footer id="footer"></Footer>
   </div>
 </template>
@@ -26,6 +47,7 @@
 import Header from './Header'
 import Footer from './Footer'
 import axios from 'axios'
+import { useVuelidate } from '@vuelidate/core'
 
 export default {
   name: 'HouseDetails',
@@ -33,6 +55,7 @@ export default {
     Header,
     Footer
   },
+  setup: () => ({ v$: useVuelidate() }),
   data () {
     return {
       logged: null,
@@ -40,7 +63,7 @@ export default {
       token: null,
       house_id: null,
       house: null,
-      titleIm: 'holaaa',
+      submitted: false,
       images: [
         {
           'itemImageSrc': 'https://doogkingteststorage.blob.core.windows.net/media/01niu-refugi_NoQaX10.jpg',
@@ -77,11 +100,24 @@ export default {
       ]
     }
   },
+  validations () {
+    return {
+    }
+  },
   methods: {
     getHouse () {
       const headers = {'Access-Control-Allow-Origin': '*'}
       const pathHouses = 'https://doogking.azurewebsites.net/api/housing/' + this.house_id + '/'
       axios.get(pathHouses, headers).then(response => (this.house = response.data))
+    },
+    handleSubmit (isFormValid) {
+      this.submitted = true
+      if (isFormValid) {
+      }
+    },
+    goToLogin () {
+      // eslint-disable-next-line standard/object-curly-even-spacing
+      this.$router.push({ path: '/login'})
     }
   },
   created () {
@@ -98,6 +134,17 @@ export default {
 </script>
 
 <style scoped>
+#houseContainer {
+  display: flex;
+}
+
+.houseDetails {
+  flex: 1;
+}
+
+.houseDetails:first-child {
+}
+
 .flex-wrapper {
   background-color: #2A323D;
   display: flex;
@@ -108,13 +155,8 @@ export default {
   align-content: center;
 }
 
-#footer{
-  flex: 1;
-}
-
 #imageHouse{
   height: 25em;
-  width: 1em;
   border-radius: 2em 2em 0 0;
   border: 5px solid #1c1b29;
 }
@@ -124,5 +166,32 @@ export default {
   width: 7em;
   border-radius: 0.5em;
   border: 3px solid #1c1b29;
+}
+
+.form-demo .card {
+  margin-top: 1.5rem;
+  border-radius: 1rem;
+  min-width: 35rem;
+  background-color: #3d4755;
+  color: white;
+  margin-bottom: 1.5rem;
+}
+.form-demo .card form{
+  margin-top: 2rem;
+}
+.form-demo .card .field{
+  margin-bottom: 1.5rem;
+  margin-left: 1rem;
+  margin-right: 1rem;
+}
+.form-demo .card .field-checkbox{
+  margin-bottom: 1.5rem;
+  margin-left: 1rem;
+  margin-right: 1rem;
+}
+.form-demo @media screen and (max-width: 960px) {
+  .card {
+    width: 80%;
+  }
 }
 </style>
