850fa44d144800a6bd5aaddd7396d59f02535881
auram <auramch98@gmail.com>
2022-11-28 04:59:24 +0100

    Backend: profile picture

--- Doogking/urls.py
+++ Doogking/urls.py
@@ -20,6 +20,9 @@ from rest_framework import routers
 from rest_framework.authtoken.views import obtain_auth_token
 from doogkingapp import views
 
+
+from django.contrib.auth import views as auth_views
+
 from django.conf.urls.static import static
 from django.conf import settings
 from django.conf.urls import url
@@ -35,5 +38,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/reset/', views.ResetView.as_view()),
     path('api/delete/<int:id>', views. ProfileViewSet.delete, name='delete'),
+    #path('api/changePassword/', auth_views.PasswordChangeView, name='password_change'),
+    path('api/uploadProfile/', views.UploaderView.as_view()),
     url(r'^.*$', TemplateView.as_view(template_name='index.html')),
 ]
--- doogkingapp/views.py
+++ doogkingapp/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.exceptions import PermissionDenied
 from rest_framework import viewsets
+from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import permissions
 from .models import Profile, Housing
 from .serializers import ProfileSerializer, HousingSerializer
+from .forms import SetPasswordForm
 import secrets
 import requests
+from django.conf import settings
+from azure.storage.blob import BlobServiceClient
 
 class ProfileViewSet(viewsets.ModelViewSet):
     queryset = Profile.objects.all().order_by('-date_joined')
@@ -18,7 +22,7 @@ class ProfileViewSet(viewsets.ModelViewSet):
             permission_classes = [permissions.AllowAny]
         else:
             permission_classes = [permissions.IsAdminUser]
-        return [permission() for permission in permission_classes]\
+        return [permission() for permission in permission_classes]
 
     @api_view(('DELETE',))
     def delete(request, id):
@@ -26,7 +30,6 @@ class ProfileViewSet(viewsets.ModelViewSet):
         if profile:
             profile.delete()
             return Response({"message": "Profile deleted"})
-
 
 class HousingViewSet(viewsets.ModelViewSet):
     queryset = Housing.objects.all()
@@ -67,8 +70,38 @@ class ResetView(APIView):
             user.set_password(request_pass)
             user.otp = ""
             user.save()
-            return Response({"message": "Password successfully changed!"})
+            SetPasswordForm
         else:
             raise PermissionDenied("Email and reset token do not match!")
 
 
+'''#@login_required
+def password_change(request):
+    user = request.user
+    if request.method == 'POST':
+        form = SetPasswordForm(user, request.POST)
+        if form.is_valid():
+            form.save()
+            return Response({"message": "Your password has been changed"})
+
+        else:
+            for error in list(form.errors.values()):
+                return Response({"message": "Your password has been changed"})
+                #messages.error(request, error)
+
+    form = SetPasswordForm(user)
+    return render(request, '', {'form': form})'''
+
+
+class UploaderView(APIView):
+
+    def post(self, request):
+        file = request.FILES['file']
+        credential = {"account_name": settings.AZURE_ACCOUNT_NAME, "account_key": settings.AZURE_ACCOUNT_KEY}
+
+        blob_service_client = BlobServiceClient("https://" + settings.AZURE_CUSTOM_DOMAIN, credential)
+
+        blob_client = blob_service_client.get_blob_client(container=settings.AZURE_CONTAINER, blob=file.name)
+        blob_client.upload_blob(file.read())
+
+        return Response({"message": "success", "uploaded_name": file.name})
