80ed5d6c5bc838aebb19ff86c630e18b62546e9d
mariaroman99 <mariaromanmartin99@gmail.com>
2022-11-29 20:04:56 +0100

    Editar Perfil boton eliminar y cambiar contraseña eliminados corregido error

--- frontend/src/components/EditProfile.vue
+++ frontend/src/components/EditProfile.vue
@@ -28,7 +28,6 @@
                 </div>
               </div>
             </div>
-            <div class="changePassword-button" align-content="left">
           </div>
         </div>
       </div>
