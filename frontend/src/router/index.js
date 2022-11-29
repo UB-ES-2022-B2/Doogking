269ae596f84484bae8071db132336130b370350a
Alicia Carrasco <48240857+aliciacarrascoo@users.noreply.github.com>
2022-11-29 23:15:04 +0100

    Update index.js

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -6,6 +6,7 @@ import process from 'shelljs'
 import Register from '../components/Register'
 import ForgotPassword from '../components/ForgotPassword'
 import Reset from '../components/Reset'
+import BusinessContact from '../components/BusinessContact'
 
 Vue.use(Router)
 
@@ -37,6 +38,11 @@ export default new Router({
       path: '/reset',
       name: 'ResetPassword',
       component: Reset
+    },
+    {
+      path: '/BusinessContact',
+      name: 'BusinessContact',
+      component: BusinessContact
     }
   ]
 })
