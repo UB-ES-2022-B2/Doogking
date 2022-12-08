28355285757cf59a81a32fc09b0ae4efdc4003ab
Aniol <aniolgarcia@gmail.com>
2022-12-08 22:01:52 +0100

    Updating admin interface to show account balance

--- doogkingapp/admin.py
+++ doogkingapp/admin.py
@@ -24,6 +24,7 @@ class ProfileAdmin(UserAdmin):
             'password',
             'first_name',
             'last_name',
+            'balance',
             'image'
         )}),
         ('Dates', {'fields': ('last_login', 'date_joined')}),
