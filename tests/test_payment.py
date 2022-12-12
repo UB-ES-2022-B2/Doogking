7dfe7beb289eddaacdd680f866e1784ada2aff23
DanielYesteLopez <dyestelo10@alumnes.ub.edu>
2022-12-12 18:48:17 +0100

    Implementing tests

--- tests/test_payment.py
+++ tests/test_payment.py
@@ -24,15 +24,39 @@ class HouseRegistryTest(StaticLiveServerTestCase):
     def test_payment_options_card_wrong_input(self):
         self.selenium.get("https://doogking.azurewebsites.net/payment?house_id=1")
         card_number = self.selenium.find_element(By.ID, "cardNumber")
-        card_number.send_keys("DD")
+        m = ''.join(chr(i) for i in range(128))
+        for characters in m:
+            card_number.send_keys(characters)
         check_code = card_number.get_attribute('value')
-        assert check_code == "____-____-____-____"
+        assert check_code == "0123-4567-89__-____"
 
     def test_payment_options_card_right_input(self):
         self.selenium.get("https://doogking.azurewebsites.net/payment?house_id=1")
         card_number = self.selenium.find_element(By.ID, "cardNumber")
-        card_number.send_keys("DD")
+        card_number.send_keys("12345")
         check_code = card_number.get_attribute('value')
-        assert check_code == "____-____-____-____"
-    def test_house_registry_fill(self):
-        assert True
+        assert check_code == "1234-5___-____-____"
+
+    def test_cvc_good(self):
+        self.selenium.get("https://doogking.azurewebsites.net/payment?house_id=1")
+        card_number = self.selenium.find_element(By.ID, "cvc")
+        card_number.send_keys("544")
+        check_code = card_number.get_attribute('value')
+        assert check_code == "544"
+
+    def test_cvc_bad(self):
+        self.selenium.get("https://doogking.azurewebsites.net/payment?house_id=1")
+        card_number = self.selenium.find_element(By.ID, "cvc")
+        m = ''.join(chr(i) for i in range(128))
+        for characters in m:
+            card_number.send_keys(characters)
+        check_code = card_number.get_attribute('value')
+        assert check_code == "012"
+
+    def test_calendar(self):
+        msg = "Expiry date has to be greater or equal than current date."
+        self.selenium.get("https://doogking.azurewebsites.net/payment?house_id=1")
+        calendar = self.selenium.find_element(By.ID, "expiryDate")
+        calendar.send_keys("12/01/2022")
+        error_msg = self.selenium.find_element(By.NAME, msg).is_displayed()
+        assert error_msg
