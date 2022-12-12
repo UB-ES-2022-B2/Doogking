9df224244714f747f001165672bec7ee2b10871d
PauRodriguezTrens <72156170+PauRodriguezTrens@users.noreply.github.com>
2022-12-12 22:51:09 +0100

    Update HouseView.vue

--- frontend/src/components/HouseView.vue
+++ frontend/src/components/HouseView.vue
@@ -26,16 +26,11 @@
             <div class="col-6 text-right">
               <DataViewLayoutOptions v-model="layout" />
             </div>
-            <Calendar class="calendarIcon" id="check_in" placeholder="Check-in" v-model="checkInDate" :showIcon="true" style="width: 9.65rem; margin-top: 0.5em; margin-right: 0.5em;"/>
-            <Calendar class="calendarIcon" id="check_out" placeholder="Check-out" v-model="checkOutDate" @click="checkOutGreater()" :showIcon="true" style="width: 9.65rem; margin-top: 0.5em; margin-right: 0.5em;"/>
+            <Calendar class="calendarIcon" id="icon" placeholder="Check-in" v-model="checkInDate" :showIcon="true" style="width: 9.65rem; margin-top: 0.5em; margin-right: 0.5em;"/>
+            <Calendar class="calendarIcon" id="icon" placeholder="Check-out" v-model="checkOutDate" :showIcon="true" style="width: 9.65rem; margin-top: 0.5em; margin-right: 0.5em;"/>
             <ConfirmPopup id="confirmPopup" ></ConfirmPopup>
-            <Toast id="error_toast"/>
+            <Toast/>
             <Button id="removeFiltersBtn" @click="confirmRemoveFilters($event)" icon="pi pi-times" style="background-color: indianred; border-color: indianred; color: white; margin-top: 0.5em;"/>
-          </div>
-          <div class="col-3 text-left">
-            <h5>Price range per day: {{priceRangeValue}}</h5>
-            <Slider id="priceRange" v-model="priceRangeValue" :step="5" :min="0" :max="500" :range="true"/>
-            <h9>Note: min: 0, max: 500</h9>
           </div>
           <Divider id="gridDivider" v-if="layout=='grid'"></Divider>
         </template>
@@ -52,35 +47,38 @@
                 </div>
               </div>
               <div class="flex flex-row md:flex-column justify-content-between w-full md:w-auto align-items-center md:align-items-end mt-5 md:mt-0">
-                  <span v-if="slotProps.data.favorite==true">
-                      <Button id="favButtonList" icon="pi pi-heart-fill" @click="changeFavorite()" class="p-button-rounded"/>
+                  <span v-if="slotProps.data.url === 'favorite'">
+                      <Button id="favButtonList" icon="pi pi-heart-fill" @click="removeFavorite(slotProps.data.house_id), slotProps.data.url = 'not favorite'" class="p-button-rounded"/>
                   </span>
                 <span v-else>
-                      <Button id="favButtonList" icon="pi pi-heart" @click="changeFavorite()" class="p-button-rounded"/>
+                      <Button id="favButtonList" icon="pi pi-heart" @click="addHouseToFavorites(slotProps.data.house_id), slotProps.data.url = 'favorite'" class="p-button-rounded"/>
                   </span>
                 <span class="text-2xl font-semibold mb-2 align-self-center md:align-self-end">{{slotProps.data.price}}€ day</span>
-                <Button id="buttonViewList" label="View house" iconPos="right" class="buttonView"/>
+                <Button id="buttonViewList" label="View house" @click="seeHouseDetails(slotProps.data.house_id)" iconPos="right" class="buttonView"/>
               </div>
             </div>
           </div>
         </template>
         <template #grid="slotProps">
-          <div class="col-12 md:col-3">
+          <div id="gridLayout" style="box-sizing: border-box;">
             <div class="card m-3 card1">
               <div id ="container-image" class="container">
                 <div id="container-effect">
-                  <img id="card-img" :src="slotProps.data.image" alt="las vegas">
+                  <img id="card-img" :src="slotProps.data.image" alt="img">
                   <figcaption>
-                    <Button id="buttonViewGrid" label="View house" class="buttonView" style="background-color: #1c1b29; color: white; border-radius: 1em; opacity: 0.7;"/>
+                    <Button id="buttonViewGrid" label="View house" @click="seeHouseDetails(slotProps.data.house_id)" class="buttonView" style="background-color: #1c1b29; color: white; border-radius: 1em; opacity: 0.7;"/>
                   </figcaption>
                 </div>
-                <span id="favContainer" v-if="slotProps.data.favorite==true">
-                        <Button id="favButtonGrid" icon="pi pi-heart-fill" @click="changeFavorite()" class="p-button-rounded"/>
-                      </span>
+                <span id="favContainer" v-if="slotProps.data.url === 'favorite'">
+                  <Button id="favButtonGrid" icon="pi pi-heart-fill" @click="removeFavorite(slotProps.data.house_id), slotProps.data.url = 'not favorite'" class="p-button-rounded"/>
+                </span>
                 <span id="favContainer" v-else>
-                        <Button id="favButtonGrid" icon="pi pi-heart" @click="changeFavorite()" class="p-button-rounded"/>
-                      </span>
+                  <Button id="favButtonGrid" icon="pi pi-heart" @click="addHouseToFavorites(slotProps.data.house_id), slotProps.data.url = 'favorite'" class="p-button-rounded"/>
+                </span>
                 <span id="priceContainer" class="text font-semibold"><a>{{slotProps.data.price}}€</a> day</span>
+                <span id="loaderContainer" v-if="loaderActive===true">
+                  <LoadingSpinnerGrid :active="true"/>
+                </span>
               </div>
               <div id="card-details" class="details">
                 <div class="flex align-items-center justify-content-between">
@@ -102,21 +100,28 @@
 
 <script>
 import axios from 'axios'
-
+import LoadingSpinnerGrid from './LoadingSpinnerGrid'
 export default {
   name: 'App',
+  components: {
+    LoadingSpinnerGrid
+  },
   data () {
     return {
-      logged: null,
-      houses: [{}],
+      logged: false,
+      username: null,
+      email: null,
+      userId: null,
+      token: null,
+      houses: [{}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}],
       layout: 'grid',
       checkInDate: null,
       checkOutDate: null,
-      selectedCities: null,
-      priceRangeValue: [0, 500],
-      minPrice: null,
-      maxPrice: null,
       h: [],
+      numHouses: null,
+      myFavorites: [{}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}],
+      loaderActive: false,
+      selectedCities: [],
       cities: [
         {name: 'Barcelona', code: 'BCN'},
         {name: 'Girona', code: 'GI'},
@@ -129,8 +134,8 @@ export default {
         {name: 'Zaragoza', code: 'ZG'},
         {name: 'Málaga', code: 'ML'},
         {name: 'Palma de Mallorca', code: 'MLL'},
-        {name: 'Las Palmas de Gran Canaria', code: 'PGC'},
-        {name: 'Hospitalet de Llobregat', code: 'HL'}
+        {name: 'Las Palmas de Gran Canaria', code: 'MLL'},
+        {name: 'Hospitalet de Llobregat', code: 'MLL'}
       ]
     }
   },
@@ -146,6 +151,7 @@ export default {
           this.selectedCities = null
           this.checkInDate = null
           this.checkOutDate = null
+          this.getHouses()
         },
         reject: () => {
         }
@@ -156,77 +162,176 @@ export default {
         this.checkOutDate = null
         this.$toast.add({severity: 'error', summary: 'Error message', detail: 'Check-out date should be greater than check-in date', life: 2000})
       }
-      if (this.houses.length !== 15) {
+      if (this.houses.length !== this.numHouses) {
         this.getHouses()
       }
-      // if (this.selectedCities.length !== 0) {
-      //  this.h.length = 0
-      //  for (let i = 0; i < this.selectedCities.length; i++) {
-      //    for (let j = 0; j < this.houses.length; j++) {
-      //      if (this.selectedCities[i].name === this.houses[j].city) {
-      //        this.h.push(this.houses[j])
-      //      }
-      //    }
-      //  } this.houses = this.h
-      // } else {
-      // this.houses.length = 0
-      this.getHouses()
-      // }
-      // this.selectedCities.length = 0
+      if (this.selectedCities.length !== 0) {
+        this.h.length = 0
+        for (let i = 0; i < this.selectedCities.length; i++) {
+          for (let j = 0; j < this.houses.length; j++) {
+            if (this.selectedCities[i].name === this.houses[j].city) {
+              this.h.push(this.houses[j])
+            }
+          }
+        } this.houses = this.h
+      } else if (this.selectedCities.length === 0) {
+        this.houses.length = 0
+        this.getHouses()
+      } this.selectedCities.length = 0
     },
     goToLogin () {
-      // eslint-disable-next-line standard/object-curly-even-spacing
-      this.$router.push({ path: '/login'})
+      this.$router.push({path: '/login'})
     },
-    changeFavorite () {
+    // eslint-disable-next-line camelcase
+    seeHouseDetails (house_id) {
+      this.$router.push({path: '/housedetails', query: {house_id: house_id}})
+    },
+    getUserFavorites () {
+      var config = {
+        method: 'get',
+        url: 'https://doogking.azurewebsites.net/api/profiles/favourites/' + this.userId + '/',
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Authorization': 'Token ' + this.token
+        }
+      }
+      axios(config)
+        .then((response) => {
+          this.myFavorites = response.data
+          this.getHouses()
+        })
+        .catch((error) => {
+          this.error = error
+        })
+    },
+    // eslint-disable-next-line camelcase
+    removeFavorite (house_id) {
       if (this.logged === false) {
-        this.$toast.add({severity: 'warn', summary: 'Warn message', detail: 'You need to login to add favorites', life: 2000})
+        this.$toast.add({severity: 'warn', summary: 'Warn message', detail: 'You need to login to add favorites.', life: 2000})
+      } else {
+        var data = JSON.stringify({
+          // eslint-disable-next-line camelcase
+          'housing': house_id,
+          'user': this.userId
+        })
+        var config = {
+          method: 'delete',
+          url: 'https://doogking.azurewebsites.net/api/favourites/',
+          headers: {
+            'Access-Control-Allow-Origin': '*',
+            'Authorization': 'Token ' + this.token,
+            'Content-Type': 'application/json'
+          },
+          data: data
+        }
+        axios(config)
+          .then((response) => {
+            this.$toast.add({severity: 'info', summary: 'Favorite', detail: 'House removed from your list of favorites.', life: 3000})
+          })
+          .catch((error) => {
+            this.error = error
+          })
+      }
+    },
+    // eslint-disable-next-line camelcase
+    addHouseToFavorites (house_id) {
+      if (this.logged === false) {
+        this.$toast.add({severity: 'warn', summary: 'Warn message', detail: 'You need to login to add favorites.', life: 2000})
+      } else {
+        var data = JSON.stringify({
+          // eslint-disable-next-line camelcase
+          'housing': 'https://doogking.azurewebsites.net/api/housing/' + house_id + '/',
+          'user': 'https://doogking.azurewebsites.net/api/profiles/' + this.userId + '/'
+        })
+        var config = {
+          method: 'post',
+          url: 'https://doogking.azurewebsites.net/api/favourites/',
+          headers: {
+            'Access-Control-Allow-Origin': '*',
+            'Authorization': 'Token ' + this.token,
+            'Content-Type': 'application/json'
+          },
+          data: data
+        }
+        axios(config)
+          .then((response) => {
+            this.$toast.add({severity: 'info', summary: 'Favorite', detail: 'House added to your favorites list. You can see it in you profile.', life: 3000})
+          })
+          .catch((error) => {
+            this.error = error
+          })
       }
     },
     getHouses () {
-      this.minPrice = this.priceRangeValue[0]
-      this.maxPrice = this.priceRangeValue[1]
-      // local: 127.0.0.1:8000, cloud: doogking.azurewebsites.net
-      var pathHouses = 'http://doogking.azurewebsites.net/api/housing/?min_price=' + this.minPrice + '&max_price=' + this.maxPrice
-      // Filter Cities
-      if (this.selectedCities !== null) {
-        for (let i = 0; i < this.selectedCities.length; i++) {
-          pathHouses += '&city=' + this.selectedCities[i].name
-        }
-      }
-      if (this.checkOutDate !== null && this.checkInDate !== null) {
-        // Check In
-        const checkInString = this.checkInDate.toString()
-        var checkIn = checkInString.substring(11, 15) + '-'
-        var month = checkInString.substring(4, 7)
-        var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
-        checkIn += (months.indexOf(month) + 1).toLocaleString('en-US', {minimumIntegerDigits: 2}) + '-' +
-          this.checkInDate.substring(8, 10)
-        // Check Out
-        const checkOutString = this.checkInDate.toString()
-        var checkOut = checkOutString.substring(11, 15) + '-'
-        month = checkOutString.substring(4, 7)
-        checkOut += (months.indexOf(month) + 1).toLocaleString('en-US', {minimumIntegerDigits: 2}) + '-' +
-          this.checkInDate.substring(8, 10)
-        pathHouses += '&check_in=' + checkIn + '&check_out=' + checkOut
-      }
+      // const pathHouses = 'http://127.0.0.1:8000/api/housing/'
       const headers = {'Access-Control-Allow-Origin': '*'}
-      axios.get(pathHouses, headers).then(response => (this.houses = response.data))
+      const pathHouses = 'https://doogking.azurewebsites.net/api/housing/'
+      axios.get(pathHouses, headers)
+        .then((response) => {
+          this.houses = response.data
+          for (let i = 0; i < this.houses.length; i++) {
+            var found = false
+            for (let j = 0; j < this.myFavorites.length && found === false; j++) {
+              if (this.houses[i].house_id === this.myFavorites[j].housing.house_id) {
+                this.houses[i].url = 'favorite'
+                found = true
+              }
+            }
+          }
+        })
+        .catch((error) => {
+          this.error = error
+        })
+    },
+    getNumHouses () {
+      const headers = {'Access-Control-Allow-Origin': '*'}
+      const pathHouses = 'https://doogking.azurewebsites.net/api/housing/'
+      const promise = axios.get(pathHouses, headers)
+      Promise.resolve(promise).then((value) => (this.numHouses = value.data.length))
+    },
+    showLoader () {
+      this.loaderActive = true
+    },
+    hideLoader () {
+      this.loaderActive = false
+    },
+    loadLocalStorage () {
+      if (localStorage.username) {
+        this.logged = true
+        this.username = localStorage.username
+      }
+      if (localStorage.userId) {
+        this.userId = localStorage.userId
+      }
+      if (localStorage.token) {
+        this.token = localStorage.token
+      }
+      if (localStorage.email) {
+        this.email = localStorage.email
+        this.getUserFavorites()
+      }
     }
   },
   created () {
-    this.logged = this.$route.query.logged === 'true'
+    this.loadLocalStorage()
     this.getHouses()
+    this.getNumHouses()
+    this.showLoader()
+    setTimeout(() => {
+      this.hideLoader()
+    }, 800)
   }
 }
 </script>
 
 <style scoped>
-
 .col-12{
   padding-bottom: 0px;
 }
-
+#gridLayout{
+  align-items: center;
+  justify-content: center;
+}
 .card{
   background-color: #1c1b29;
   border-radius: 20px;
@@ -236,22 +341,23 @@ export default {
   box-sizing: border-box;
   overflow: hidden;
   text-overflow: ellipsis;
+  width: 22.3vw;
+  height: 22vw;
 }
-
 #container-image{
   position: relative;
   box-sizing: border-box;
   padding: 0;
   margin: 0;
 }
-
 #card-img{
   clip-path: polygon(0 0,100% 0, 100% 85%, 0 100%);
-  width: 100%;
   display: block;
   border-radius: 20px 20px 0 0;
+  overflow: hidden;
+  width: 22.3vw;
+  height: 14vw;
 }
-
 #container-effect {
   background-color: #000;
   display: inline-block;
@@ -259,14 +365,12 @@ export default {
   border-radius: 20px 20px 0 0;
   clip-path: polygon(0 0,100% 0, 100% 85%, 0 100%);
 }
-
 #container-effect * {
   -webkit-box-sizing: border-box;
   box-sizing: border-box;
   -webkit-transition: all 0.35s ease;
   transition: all 0.35s ease;
 }
-
 #container-effect:before,
 #container-effect:after {
   position: absolute;
@@ -283,17 +387,14 @@ export default {
   opacity: 0.6;
   z-index: 1;
 }
-
 #container-effect:before {
   -webkit-transform: skew(45deg) translateX(-155%);
   transform: skew(45deg) translateX(-155%);
 }
-
 #container-effect:after {
   -webkit-transform: skew(45deg) translateX(155%);
   transform: skew(45deg) translateX(155%);
 }
-
 #container-effect figcaption {
   top: 50%;
   left: 50%;
@@ -303,30 +404,25 @@ export default {
   transform: translate(-50%, -50%) scale(0.5);
   opacity: 0;
 }
-
 #container-effect:hover > #card-img,
 #container-effect.hover > #card-img {
   opacity: 0.5;
 }
-
 #container-effect:hover:before{
   -webkit-transform: skew(45deg) translateX(-55%);
   transform: skew(45deg) translateX(-55%);
 }
-
 #container-effect:hover:after,
 #container-effect.hover:after {
   -webkit-transform: skew(45deg) translateX(55%);
   transform: skew(45deg) translateX(55%);
 }
-
 #container-effect:hover figcaption,
 #container-effect.hover figcaption {
   -webkit-transform: translate(-50%, -50%) scale(1);
   transform: translate(-50%, -50%) scale(1);
   opacity: 1;
 }
-
 #card-details{
   padding: 20px 10px;
 }
@@ -341,33 +437,28 @@ export default {
   line-height: 30px;
   font-weight: 400;
 }
-
 #buttonViewList{
   background-color: #6c757d;
   border-color: #6c757d;
   color: white;
 }
-
 #buttonViewGrid{
   background-color: #6c757d;
   border-color: #6c757d;
   color: white;
 }
-
 #buttonViewList:hover{
   background-color: #8DD0FF;
   border-color: #8DD0FF;
   outline-color: #8DD0FF;
   color: white;
 }
-
 #buttonViewGrid:hover{
   background-color: #8DD0FF;
   border-color: #8DD0FF;
   outline-color: #8DD0FF;
   color: white;
 }
-
 #buttonViewList:focus {
   box-shadow: 0 0 0 0.1em #8DD0FF;
   background-color: #8DD0FF;
@@ -375,7 +466,6 @@ export default {
   outline-color: #8DD0FF;
   color: white;
 }
-
 #buttonViewGrid:focus {
   box-shadow: 0 0 0 0.1em #8DD0FF;
   background-color: #8DD0FF;
@@ -383,36 +473,29 @@ export default {
   outline-color: #8DD0FF;
   color: white;
 }
-
 #listImage{
   border-radius: 0.5em;
   border: 3px solid #1c1b29;
 }
-
 .p-dropdown {
   width: 14rem;
   font-weight: normal;
 }
-
 .product-name {
   font-size: 1.5rem;
   font-weight: 700;
 }
-
 .product-description {
   margin: 0 0 1rem 0;
 }
-
 .product-category-icon {
   vertical-align: middle;
   margin-right: .5rem;
 }
-
 .product-category {
   font-weight: 600;
   vertical-align: middle;
 }
-
 .multiselect-custom >>> .country-item-value {
   font-size: 0.8rem;
   padding: 0.1rem;
@@ -422,66 +505,60 @@ export default {
   background-color: #6c757d;
   color: white;
 }
-
 #tagHost{
   position:absolute;
   top:1em;
   left:1em;
 }
-
 #favContainer{
   position:absolute;
   top:0.5em;
   right:0.5em;
 }
-
+#loaderContainer{
+  position:absolute;
+  top:40%;
+  left:45%;
+}
 #priceContainer{
   position:absolute;
   top:90%;
   right:0.5em;
   font-size: 1em;
 }
-
 #priceContainer a{
   font-size: 1.5em;
 }
-
 #calendarIcon >>> p-calendar >>> p-calendar-w-btn{
   bacground-color: white;
   color: white;
 }
-
 #favButtonGrid{
   color: indianred;
   background-color: #2A323D;
   border-color: #2A323D;
 }
-
 #favButtonGrid:hover{
   color: indianred;
   background-color: #2A323D;
   border-color: #2A323D;
 }
-
 #favButtonGrid:focus{
   color: indianred;
   background-color: #2A323D;
   border-color: #2A323D;
   box-shadow: 0 0 0 0.1em indianred;
 }
-
 #favButtonList{
   color: indianred;
   background-color: #1c1b29;
   border-color: #1c1b29;
 }
-
 #favButtonList:hover{
   color: indianred;
   background-color: #1c1b29;
   border-color: #1c1b29;
 }
-
 #favButtonList:focus{
   color: indianred;
   background-color: #1c1b29;
